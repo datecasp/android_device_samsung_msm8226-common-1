d4b8b509c75a1620e1c757f6ea6a94cfe9a49630
sub77 <sub77@ymail.com>
2016-08-08 20:07:03 +0200

    Update Patchbase

--- patchsetup.sh
+++ patchsetup.sh
@@ -11,22 +11,22 @@ git fetch https://github.com/sub77-du/android_packages_apps_Camera2.git  && git 
 cd ..;cd ..; cd .. # End packages/apps/Camera2
 
 # Start packages/apps/Snap
-cd packages/apps/Snap
-git checkout sub/m-caf-samsung
-cd ..;cd ..; cd .. # End packages/apps/Snap
+#cd packages/apps/Snap
+#git checkout sub/m-caf-samsung
+#cd ..;cd ..; cd .. # End packages/apps/Snap
 
 # End Device specif branches
 
 # Start frameworks/base
-cd frameworks/base
-git pull https://github.com/sub77-du/android_frameworks_base.git
-cd ..;cd .. # End frameworks/base
+#cd frameworks/base
+#git pull https://github.com/sub77-du/android_frameworks_base.git
+#cd ..;cd .. # End frameworks/base
 
 # Start packages/apps/Settings
-cd packages/apps/Settings
-git pull https://github.com/sub77-du/android_packages_apps_Settings.git
-cd ..;cd ..; cd .. # End packages/apps/Settings
+#cd packages/apps/Settings
+#git pull https://github.com/sub77-du/android_packages_apps_Settings.git
+#cd ..;cd ..; cd .. # End packages/apps/Settings
 
 
 # Run device patches on the tree
-sh device/samsung/msm8226-common/patch/apply.sh
+#sh device/samsung/msm8226-common/patch/apply.sh
